fefc03b2ddffea2754f09773db245a7a0499b3be
Erika <upsk252g3h355@iscte-iul.pt>
2026-04-16 20:53:00 +0100

    Corrige DTOs vazios
    Added required properties for Nome and Email.

--- BibliotecaAPI/DTOs/CriarUsuarioDTO.cs
+++ BibliotecaAPI/DTOs/CriarUsuarioDTO.cs
@@ -2,5 +2,8 @@
 {
     public class CriarUsuarioDTO
     {
+        public required string Nome { get; set; }  // required: Isso obriga, quem criar o objeto a preencher
+        public required string Email { get; set; }
+        public int TipoUsuarioID { get; set; }
     }
 }
