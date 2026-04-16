3e798532ff439594c211cf05c73c54d3b3822ba2
Erika <upsk252g3h355@iscte-iul.pt>
2026-04-16 20:50:21 +0100

    Corrige DTOs vazios

--- BibliotecaAPI/DTOs/AtualizarObraDTO.cs
+++ BibliotecaAPI/DTOs/AtualizarObraDTO.cs
@@ -2,5 +2,9 @@
 {
     public class AtualizarObraDTO
     {
+        public required string Titulo { get; set; }  
+        public required string Autor { get; set; }
+        public int GeneroID { get; set; }
+        public required string Descricao { get; set; }  
     }
 }
