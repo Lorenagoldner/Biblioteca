7c624ca8b86b6c3af1498fa3f4e68175af91c18e
Erika <upsk252g3h355@iscte-iul.pt>
2026-04-16 20:56:04 +0100

    Corrige DTOs vazios

--- BibliotecaAPI/DTOs/NucleoDTO.cs
+++ BibliotecaAPI/DTOs/NucleoDTO.cs
@@ -2,5 +2,8 @@
 {
     public class NucleoDTO
     {
+        public int ID { get; set; }
+        public required string Nome { get; set; }
+        public required string Morada { get; set; }
     }
 }
